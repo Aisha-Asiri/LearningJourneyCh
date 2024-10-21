f4c77ca1452cf5a3f6191a494cd2bd846d563845
Aisha-Asiri <travel2paradise2@gmail.com>
2024-10-21 09:44:46 +0300

    Initial Commit

---
+++ LearningJourneyCh/LearningJourneyChApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyChApp.swift
+//  LearningJourneyCh
+//
+//  Created by Aisha Asiri on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyChApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
